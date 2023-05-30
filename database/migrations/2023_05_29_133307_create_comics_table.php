179ae9494a18f1e722c42490b26f11d48eb25446
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-05-30 16:36:10 +0200

    settings migrations

--- database/migrations/2023_05_29_133307_create_comics_table.php
+++ database/migrations/2023_05_29_133307_create_comics_table.php
@@ -16,11 +16,11 @@ return new class extends Migration
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
             $table->string('title', 100);
-            $table->text('description');
-            $table->string('thumb');
-            $table->decimal('price', 8, 2);
+            $table->text('description', 255);
+            $table->text('thumb', 255);
+            $table->string('price');
             $table->string('series')->nullable();
-            $table->string('type');
+            $table->string('type',255);
             $table->timestamps();
         });
     }
