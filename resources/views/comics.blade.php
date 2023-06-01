73add8500f0231e0d84167d6b36ebb2230fa1fed
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-06-01 12:05:45 +0200

    add graphic details

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -8,12 +8,15 @@
                     <div class="col my-3">
                         <div class="card h-100 shadow">
                             <img src="{{ $comic->thumb }}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $comic->title }}</h5>
+                            <div class="card-body bg-dark">
+                                <h5 class="card-title text-white">{{ $comic->title }}</h5>
                                 <h6 class="card-subtitle mb-2 text-muted ">{{ $comic->price }}</h6>
-                                <p class="card-text">{{ $comic->type }}</p>
-                                <p class="card-text">{{ $comic->series }}</p>
-                                <a class="btn btn-primary" href="{{ route('admin.comics.show', $comic->id) }}"
+                                <p class="card-text text-white">{{ $comic->type }}</p>
+                                <p class="card-text text-white">{{ $comic->series }}</p>
+                                
+                            </div>
+                            <div class="bg-dark text-center border-top-0 rounded-bottom py-2">
+                                <a class="btn btn-danger  " href="{{ route('admin.comics.show', $comic->id) }}"
                                     role="button">View</a>
                             </div>
                         </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,22 +5,22 @@
 
 
     <div class="bg-dark py-2">
-        <div class="container py-3">
+        <div class="container py-3 negative_margin">
 
             <strong class="bg-primary text-white my_span text-uppercase my-3">current sereis</strong>
 
             <div class="row row-cols-md-6 my-3">
                 @forelse ($comics as $comic)
                     <div class="col my-2">
-                        <div class="card h-100 rounded-0">
-                            <img src="{{ $comic->thumb }}" class="img-fluid" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $comic->title }}</h5>
+                        <div class="card h-100">
+                            <img src="{{ $comic->thumb }}" class="my_home_img card-img-top" alt="...">
+                            <div class="card-body bg-dark">
+                                <h5 class="card-title text-white">{{ $comic->title }}</h5>
                                 <h6 class="card-subtitle text-muted ">{{ $comic->price }}</h6>
 
                             </div>
-                            <div class="card-footer">
-                                <a class="btn btn-primary" href="{{ route('admin.comics.show', $comic->id) }}"
+                            <div class="bg-dark text-center border-top-0 rounded-bottom py-2">
+                                <a class="btn btn-danger  " href="{{ route('admin.comics.show', $comic->id) }}"
                                     role="button">View</a>
                             </div>
                         </div>
